a2597a24e0538554f24da655ebec80c2083bd746
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-22 13:37:17 -0300

    Projeto inicial finalizado!!!

--- script.js
+++ script.js
@@ -53,5 +53,24 @@ function excluirTarefa(id) {
 
 
 function marcarTarefa(id) {
-    
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+
+
+    if(classe == "item") {
+
+        item.classList.add('clicado');
+
+        var icone = document.getElementById('icone_' + id);
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+
+        item.parentNode.appendChild(item);
+    } else {
+        item.classList.remove('clicado');
+
+        var icone = document.getElementById('icone_' + id);
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    }
 }
