561910098803154e552f6fc5b0152a529f7d9815
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-22 11:39:03 -0300

    1 hora

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-
+var contador = 0;
 
 
 function adicionarTarefa() {
-    let tarefa = document.getElementById('inputTarefa').value;
-    alert(tarefa)
+    let resultado = document.getElementById('areaLista');
+    let input = document.getElementById('inputTarefa');
+    let newTarefa;
+    let valorInput = input.value;
+
+    if (inputVazio(valorInput) == true) {
+        alert('Digite a Tarefa por favor');
+
+    } else {
+        contador++;
+        newTarefa =
+        `<div id="${contador}" class="item">
+            <div onclick="marcarTarefa(${contador})" class="item-icone">
+                <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+            </div>
+            <div onclick="marcarTarefa(${contador})" class="item-nome">
+                ${valorInput}
+            </div>
+            <div class="item-button">
+                <button onclick="excluirTarefa(${contador})" id="btnexcluir" class="delete" "><i class="mdi mdi-delete"></i>Excluir</button>
+            </div>
+        </div>
+        `;
+
+        resultado.innerHTML += newTarefa;
+
+        input.value = '';
+        input.focus();
+    }
+
+}
+
+
+function inputVazio(valorInput) {
+    if (valorInput == '' || valorInput == null || valorInput == undefined) {
+        input = document.getElementById('inputTarefa');
+        input.focus();
+        return true;
+
+    }
+    return false;
+}
+
+
+function excluirTarefa(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+
+function marcarTarefa(id) {
+    
 }
